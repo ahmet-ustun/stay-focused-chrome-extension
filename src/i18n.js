10c7c944f88fb47e3f08d6dbcc4b95f7b058b95d
Ahmet Ustun <austu@softserveinc.com>
2024-11-05 17:24:28 +0200

    Refactor i18n implementation to use external JSON for English translations

--- src/i18n.js
+++ src/i18n.js
@@ -1,15 +1,12 @@
 import Vue from "vue";
 import VueI18n from "vue-i18n";
 
+import en from "./locales/en.json";
+
 Vue.use(VueI18n);
 
 const messages = {
-  en: {
-    message: { blockByWebsite: "Block by Website" },
-  },
-  fr: {
-    message: { blockByWebsite: "Bloquer par site Web" },
-  },
+  en,
 };
 
 const i18n = new VueI18n({
