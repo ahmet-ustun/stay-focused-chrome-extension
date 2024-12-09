5c2bd6fadbb3adc05b08d313062948de2315d5f0
Ahmet Ustun <austu@softserveinc.com>
2024-12-09 17:23:38 +0200

    Add EN, DE, ES, FR, IT and TR translations for UI texts

--- src/i18n.js
+++ src/i18n.js
@@ -2,11 +2,21 @@ import Vue from "vue";
 import VueI18n from "vue-i18n";
 
 import en from "./locales/en.json";
+import de from "./locales/de.json";
+import fr from "./locales/fr.json";
+import es from "./locales/es.json";
+import it from "./locales/it.json";
+import tr from "./locales/tr.json";
 
 Vue.use(VueI18n);
 
 const messages = {
   en,
+  de,
+  fr,
+  es,
+  it,
+  tr,
 };
 
 const i18n = new VueI18n({
