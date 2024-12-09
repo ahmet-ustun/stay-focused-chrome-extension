daf8f4332fa1766621ebf20b47b099107c639227
Ahmet Ustun <austu@softserveinc.com>
2024-12-09 23:05:42 +0200

    Persist selected language in localStorage and update locale initialization

--- src/i18n.js
+++ src/i18n.js
@@ -19,10 +19,20 @@ const messages = {
   tr,
 };
 
+const savedLocale = localStorage.getItem('language') || 'en';
+
 const i18n = new VueI18n({
-  locale: "en",
+  locale: savedLocale,
   fallbackLocale: "en",
   messages,
 });
+
+i18n.watchLocale = () => {
+  i18n._vm.$watch('locale', (newLocale) => {
+    localStorage.setItem('language', newLocale);
+  });
+};
+
+i18n.watchLocale();
 
 export default i18n;
