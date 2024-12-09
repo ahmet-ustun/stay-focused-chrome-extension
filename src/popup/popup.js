ffad58a78941ff3e1b2abe2b5329d7d4c59df767
Ahmet Ustun <austu@softserveinc.com>
2024-12-09 23:05:32 +0200

    Integrate i18n support into the popup component for multilingual rendering

--- src/popup/popup.js
+++ src/popup/popup.js
@@ -13,6 +13,8 @@ import {
 import "vue-material/dist/vue-material.min.css";
 import "vue-material/dist/theme/default.css";
 
+import i18n from "../i18n.js";
+
 Vue.use(MdSwitch);
 Vue.use(MdButton);
 Vue.use(MdCard);
@@ -21,5 +23,6 @@ Vue.use(MdBadge);
 /* eslint-disable no-new */
 new Vue({
   el: "#app",
-  render: h => h(App)
+  render: h => h(App),
+  i18n
 });
